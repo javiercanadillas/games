b0e03e5bbb93546c5bd7b3a3e7b13021beb02399
Javier Cañadillas <javiercm@google.com>
2026-01-22 17:55:13 +0100

    step-07-create-server-py

---
+++ tetris/server.py
@@ -0,0 +1,18 @@
+import http.server
+import socketserver
+import os
+
+PORT = 8080
+
+class TetrisHandler(http.server.SimpleHTTPRequestHandler):
+    def do_GET(self):
+        if self.path == '/':
+            self.path = '/index.html'
+        return http.server.SimpleHTTPRequestHandler.do_GET(self)
+
+
+os.chdir(os.path.join(os.path.dirname(__file__)))
+
+with socketserver.TCPServer(("", PORT), TetrisHandler) as httpd:
+    print(f"serving at port {PORT}")
+    httpd.serve_forever()
