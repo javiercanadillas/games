d9ac98404b47d6330f6d9e0bca7c6e42bf6e1283
Javier Cañadillas <javiercm@google.com>
2026-01-22 17:56:14 +0100

    fix: server.py to correctly serve files

--- tetris/server.py
+++ tetris/server.py
@@ -5,13 +5,13 @@ import os
 PORT = 8080
 
 class TetrisHandler(http.server.SimpleHTTPRequestHandler):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, directory=os.path.join(os.path.dirname(__file__)), **kwargs)
+
     def do_GET(self):
         if self.path == '/':
             self.path = '/index.html'
         return http.server.SimpleHTTPRequestHandler.do_GET(self)
-
-
-os.chdir(os.path.join(os.path.dirname(__file__)))
 
 with socketserver.TCPServer(("", PORT), TetrisHandler) as httpd:
     print(f"serving at port {PORT}")
